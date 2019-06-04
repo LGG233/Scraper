31c8cd32bc1f97be9f3060111f1ae736685d6bad
Fred Astaire <lancebc@thegodardgroup.com>
2019-06-04 16:42:41 -0400

    minor fix to server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
+require("./Routes/apiRoutes")(app);
 
 // Start the server
 app.listen(PORT, function () {
